a5544a2b0b4144bb1a480b5bc4b89425a17b68af
Arif Hanif <arif.hanif3@gmail.com>
2021-07-07 14:36:11 -0400

    update to latest HC package

--- src/schema-first/Program.cs
+++ src/schema-first/Program.cs
@@ -1,9 +1,16 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
+using Neo4j.Driver;
+
+
+IDriver driver = GraphDatabase.Driver(
+    "neo4j://localhost:7687", 
+    AuthTokens.Basic("neo4j", "test123"));
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services
+    .AddSingleton(driver)
     .AddGraphQLServer()
     .AddQueryType()
     .AddMovieLibraryTypes();
