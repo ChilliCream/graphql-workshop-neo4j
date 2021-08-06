6af1602d4baaa59cad04183e5d9f8c34c2a3051c
Arif Hanif <arif.hanif3@gmail.com>
2021-08-06 18:41:03 -0400

    code first update

---
+++ src/code-first/Models/Actor.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using HotChocolate.Data.Neo4J;
+
+namespace MoviesAPI.Models
+{
+    public class Actor
+    {
+        public string Name { get; set; }
+
+        [Neo4JRelationship("ACTED_IN")]
+        public List<Movie> ActedIn { get; set; }
+    }
+}
